41b8c6313c281e94db604ae43b9987c1796702ba
Caio Mora <c.mora.alexandre@gmail.com>
2022-10-26 22:04:20 -0300

    (fix) correções de comentários

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -11,27 +11,30 @@ export class DashboardService {
 
   constructor(private http: HttpClient) {}
 
-  // GET na API para que retorne todas as tarefas registradas no db.json
+  // method(GET)
+  // Retorna todas as tarefas cadastradas no db.json
   getFirstTask() {
     return this.http.get<Task>(`${this.API}`).pipe(delay(2000));
   }
-
-  //POST na API para que crie novas tarefas
+  // method(POST)
+  // Cria uma nova tarefa no db.json
   createTask(task: any) {
     return this.http.post(`${this.API}`, task).pipe(take(1));
   }
 
-  //GET de id da tarefa
+  // method(GET)
+  // Edita uma tarefa cadastrada no db.json passando o id na rota
   editTask(id: any) {
     return this.http.get(`${this.API}/${id}`).pipe(take(1));
   }
 
-  //PUT na API para editar uma tarefa com base no id selecionado
+  // method(PUT)
+  // Atualiza uma tarefa no db.json
   updateTask(task: any) {
     return this.http.put(`${this.API}/${task.id}`, task).pipe(take(1));
   }
-
-  //DELETE na API para excluir uma tarefa
+  // method(DELTE)
+  // Deleta uma tarefa existente no db.json
   deleteTask(id: any) {
     return this.http.delete(`${this.API}/${id}`).pipe(take(1));
   }
